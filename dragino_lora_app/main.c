ff5a03bbac2a77ce70dffa195a065976ef686df2
campbell cairns <cam.cairns@verizon.com>
2019-07-02 17:21:41 +0100

    Compiles

--- dragino_lora_app/main.c
+++ dragino_lora_app/main.c
@@ -318,6 +318,35 @@ boolean receive(char *payload)
     }
     return true;
 }
+float FuelLevel(int sensVal)
+{
+/* 	Piecewise linearization of fuel sensor reading (ref spreadsheet for calculation)
+	M           C             Threshold
+	-----------------------------------
+	55.9678	    -10.0987  
+	72.2230	    -31.3314      1.235
+	30.2275	    3.9530        0.841
+
+ */
+    //Check voltage is within range
+    
+	//convert sens val to float
+	float sens = sensVal/1024.0; //2^10
+	float perc;
+	if (sens< 0.841)
+	{
+		perc = sens*30.2275 + 3.9530;
+	}
+	else if (sens < 1.235)
+	{
+		perc = sens*(72.2230) - 31.3314;
+	}
+	else
+	{
+		perc = sens*55.9678 - 10.0987;
+	}
+	return perc;
+}
 
 void receivepacket() 
 {
@@ -424,35 +453,6 @@ void txlora(byte *frame, byte datalen)
     opmode(OPMODE_TX);
 
     printf("send: %s\n", frame);
-}
-float FuelLevel(int sensVal)
-{
-/* 	Piecewise linearization of fuel sensor reading (ref spreadsheet for calculation)
-	M           C             Threshold
-	-----------------------------------
-	55.9678	    -10.0987  
-	72.2230	    -31.3314      1.235
-	30.2275	    3.9530        0.841
-
- */
-    //Check voltage is within range
-    
-	//convert sens val to float
-	float sens = sensVal/1024.0; //2^10
-	float perc;
-	if sens< 0.841
-	{
-		perc = sens*30.2275 + 3.9530;
-	}
-	else if sense < 1.235
-	{
-		perc = sense*(72.2230) - 31.3314;
-	}
-	else
-	{
-		perc = sense*55.9678 - 10.0987;
-	}
-	return perc;
 }
 
 int main (int argc, char *argv[]) 
